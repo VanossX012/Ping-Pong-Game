2f23d6874b8fe1ab68038885379b05a0a80aa7a3
VanossX012 <103306159+VanossX012@users.noreply.github.com>
2022-12-28 21:54:51 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,45 @@
+import Ball from './Ball.js'
+import Paddle from './Paddle.js'
+
+const ball = new Ball(document.getElementById("ball"))
+
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+
+const playerScoreElem = document.getElementById("Player-Score")
+
+const computerScoreElem = document.getElementById("Computer-Score")
+
+let lastTime
+function update(time){
+    if (lastTime != null){
+    const delta = time - lastTime
+ ball.update(delta, [playerPaddle.rect(),computerPaddle.rect()])
+    computerPaddle.update(delta,ball.y)
+const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"))
+document.documentElement.style.setProperty("--hue",hue + delta * .01 )    
+if (isLose())
+        handleLose()
+}
+    lastTime=time
+    window.requestAnimationFrame(update)
+}
+function isLose(){
+    const rect = ball.rect()
+    return rect.left >= window.innerWidth || rect.right <= 0
+}
+ function handleLose(){
+     const rect = ball.rect()
+     if(rect.right >= window.innerWidth){
+         playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    } else {
+         computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1 
+    }
+     ball.reset()
+     computerPaddle.reset()
+ }
+document.addEventListener("mousemove",e =>{
+    playerPaddle.position = (e.y/window.innerHeight) * 100
+})
+window.requestAnimationFrame(update)
